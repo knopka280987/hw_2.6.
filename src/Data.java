4cf876f87b51218f4607bd870fda07b6051c9325
Татьяна <fokT.M@yandex.ru>
2022-10-19 08:57:13 +0300

    Commit1

---
+++ src/Data.java
@@ -0,0 +1,12 @@
+public class Data {
+
+    private Data() {
+    }
+    public static boolean check(String login,
+                                String password,
+                                String confirmPassword) {
+
+        return false;
+    }
+
+}
